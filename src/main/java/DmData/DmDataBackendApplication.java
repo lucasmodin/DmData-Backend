429889282976f85968dbeac361a7698e410ccdac
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-05-05 13:15:00 +0200

    Initial commit

---
+++ src/main/java/DmData/DmDataBackendApplication.java
@@ -0,0 +1,13 @@
+package DmData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmDataBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmDataBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/DmData/DmDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package DmData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmDataBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
