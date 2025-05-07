29da8ae1224f378dbe0049d7dc4498870e3c606e
OSBJE <osbj0001rstud.kea.dk>
2025-05-07 12:16:47 +0200

    API call set up aswell

--- src/main/java/DmData/message/MessageController.java
+++ src/main/java/DmData/message/MessageController.java
@@ -1,12 +1,34 @@
 package DmData.message;
 
 
+import DmData.message.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("")
+@CrossOrigin("*")
 public class MessageController {
 
+
+    private MessageService messageService;
+
+    public MessageController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @PostMapping("saveMessage")
+    public ResponseEntity<Message> saveMessage(@RequestBody Message msg){
+        Message obj = new Message(msg.getName(), msg.getMail(), msg.getMessage());
+        if (msg.getNumber() != null){
+            obj.setNumber(msg.getNumber());
+        }
+        messageService.saveMessage(obj);
+
+        return ResponseEntity.ok(obj);
+    }
 
 
 }
--- src/main/java/DmData/message/MessageService.java
+++ src/main/java/DmData/message/MessageService.java
@@ -1,10 +1,24 @@
 package DmData.message;
 
 
+import DmData.message.model.Message;
+import DmData.message.repositoryMessage.MessageHibernateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
+
+    private MessageHibernateRepository messageDB;
+
+    public MessageService(MessageHibernateRepository messageDB){
+        this.messageDB = messageDB;
+    }
+
+    public void saveMessage(Message msg){
+        messageDB.save(msg);
+    }
 
 
 }
