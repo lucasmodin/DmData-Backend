5f64c185ff984560782a3f51bd1ad8dade765fdc
OSBJE <osbj0001rstud.kea.dk>
2025-05-08 13:45:51 +0200

    Working mailsystem

--- src/main/java/DmData/message/mail/MailService.java
+++ src/main/java/DmData/message/mail/MailService.java
@@ -2,39 +2,39 @@ package DmData.message.mail;
 
 import DmData.message.model.Message;
 import jakarta.annotation.PostConstruct;
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class mailService {
+public class MailService {
 
     private final JavaMailSender javaMailSender;
-    private String mail = "lucasmodj@gmail.com";
+    private String mail = "osbj0001@stud.kea.dk";
 
-    public mailService(JavaMailSender javaMailSender) {
+    public MailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     @PostConstruct
     public void test(){
-        this.sendEmail(new Message("Oskar Bjerborg", "gay@mail.com", "I like big booty"));
+        this.sendEmail(new Message("Oskar Bjerborg", "gay@mail.com","25672072" ,"I like big booty"));
     }
 
     public void sendEmail(Message msg){
         SimpleMailMessage email = new SimpleMailMessage();
 
         String message ="Hey DM DataScience,\n" +
-                "\n" + msg.getName() + " has send you an message" +
+                "\n" + msg.getName() + " from big ass company ?\n" +
                 "\n" + msg.getMessage() + "\n" +
-                "You can also call him on: " + msg.getNumber() + "or mail: " + msg.getMail()
-                + "Best regards \n You homepage !";
+                "\nYou can reach him on: " + msg.getNumber() + " or mail: " + msg.getMail() + "\n"
+                + "\nBest regards" +
+                "\nYou homepage !";
 
         email.setFrom("mySystem");
         email.setTo(mail);
-        email.setSubject( msg.getName() + "from: " + "?" + "wants to contact you");
+        email.setSubject( msg.getName() + " from: " + "?" + " have send an message");
         email.setText(message);
         javaMailSender.send(email);
     }
