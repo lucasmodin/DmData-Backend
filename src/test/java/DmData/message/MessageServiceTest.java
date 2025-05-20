a50869af726314ed44362525cd8f9dca83f10121
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2025-05-20 09:41:05 +0200

    Cleaning up tests

--- src/test/java/DmData/message/MessageServiceTest.java
+++ src/test/java/DmData/message/MessageServiceTest.java
@@ -36,14 +36,7 @@ class MessageServiceTest {
         verify(messageDB, times(1)).save(msg);
     }
 
-    //man kan godt gemme samme besked flere gange.
-    @Test
-    void saveMessage_callsRepositorySaveMultipleTimes() {
-        Message msg = new Message("Test", "test@mail.com", "1234567890", "Hello");
-        messageService.saveMessage(msg);
-        messageService.saveMessage(msg);
-        verify(messageDB, times(2)).save(msg);
-    }
+
 
     @Test
     void sendAutoMail_callsMailServiceSendEmailOnce() {
@@ -52,58 +45,12 @@ class MessageServiceTest {
         verify(mailService, times(1)).sendEmail(msg);
     }
 
-    // --- Man kan godt gemme beskeder i databasen uden validering fra service---
-    @Test
-    void saveMessage_emptyFields_saveCalled() {
-        Message msg1 = new Message("", "test@mail.com", "1234567890", "Besked");
-        Message msg2 = new Message("Firma", "", "1234567890", "Besked");
-        Message msg3 = new Message("Firma", "test@mail.com", "", "Besked");
-        Message msg4 = new Message("Firma", "test@mail.com", "1234567890", "");
 
-        messageService.saveMessage(msg1);
-        messageService.saveMessage(msg2);
-        messageService.saveMessage(msg3);
-        messageService.saveMessage(msg4);
-
-        // Forvent save kald 1 gang per besked
-        verify(messageDB, times(4)).save(any(Message.class));
-    }
-
-    @Test
-    void saveMessage_phoneNumberTooShort_saveCalled() {
-        Message msg = new Message("Firma", "test@mail.com", "1234", "Besked");
-        messageService.saveMessage(msg);
-        verify(messageDB, times(1)).save(msg);
-    }
-
-    //Backenden har ingen validering til telefon numerne når de er nået forbi frontenden
-    @Test
-    void saveMessage_phoneNumberTooLong_saveCalled() {
-        Message msg = new Message("Firma", "test@mail.com", "123456789012", "Besked");
-        messageService.saveMessage(msg);
-        verify(messageDB, times(1)).save(msg);
-    }
 
     //Beskeder kan godt være et enkelt anslag
     @Test
     void saveMessage_singleCharacterMessage_saveCalled() {
         Message msg = new Message("Firma", "test@mail.com", "1234567890", "1");
-        messageService.saveMessage(msg);
-        verify(messageDB, times(1)).save(msg);
-    }
-
-    //Denne test er mest til hvis man når forbi HTML validering i frontenden
-    @Test
-    void saveMessage_phoneNumberNotNumeric_saveCalled() {
-        Message msg = new Message("Firma", "test@mail.com", "ABCD", "Besked");
-        messageService.saveMessage(msg);
-        verify(messageDB, times(1)).save(msg);
-    }
-
-    //Denne test er mest til hvis man når forbi HTML validering i frontenden
-    @Test
-    void saveMessage_phoneNumberAlphanumeric_saveCalled() {
-        Message msg = new Message("Firma", "test@mail.com", "HEJ1", "Besked");
         messageService.saveMessage(msg);
         verify(messageDB, times(1)).save(msg);
     }
