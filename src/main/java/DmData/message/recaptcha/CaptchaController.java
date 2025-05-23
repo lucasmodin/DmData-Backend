15cc0827ea65ffb41aa2782acc2a51f8eebd9500
lucasmodin <lumo0002@stud.kea.dk>
2025-05-23 17:18:39 +0200

    cors fix

--- src/main/java/DmData/message/recaptcha/CaptchaController.java
+++ src/main/java/DmData/message/recaptcha/CaptchaController.java
@@ -1,23 +1,25 @@
 package DmData.message.recaptcha;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
+
 public class CaptchaController {
 
-    @Autowired
+
     private Verifycaptcha verifycaptcha;  // service filen
+
+    public CaptchaController(Verifycaptcha verifycaptcha) {
+        this.verifycaptcha = verifycaptcha;
+    }
 
     @PostMapping("/verify-captcha")
     public ResponseEntity<?> handleFormSubmission(@RequestParam("g-recaptcha-response") String captchaResponse) {
         if (!verifycaptcha.verifyCaptcha(captchaResponse)) {  //hvis verificeringen fejler
             return ResponseEntity.badRequest().body("Captcha validation failed");
+
         }
         return ResponseEntity.ok("Success");
     }
